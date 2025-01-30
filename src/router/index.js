f41dca9d1e48cea9ab53728a340d7bfed7e116a1
DonArtkins <info.sycx.ke@gmail.com>
2025-01-30 17:03:54 +0300

    updated the rotes and included my own routes and updated the title and favicon and added images in public folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { HomeView, LoginView } from "../views";
-import { useAuthStore } from "../store";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
-const routes = [
-  { path: "/", name: "home", component: HomeView },
-  { path: "/login", name: "login", component: LoginView },
-];
-
-/**Initialize here */
-const router = createRouter({ history: createWebHistory(), routes });
-
-router.beforeEach(async (to) => {
-  // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ["/login"];
-  const authRequired = !publicPages.includes(to.path);
-  const auth = useAuthStore();
-
-  /**add function to check if user is logged in */
-
-  if (authRequired && !auth.user) {
-    auth.returnUrl = to.fullPath;
-    return "/login";
-  }
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: HomeView },
+    // { path: '/about', component: () => import('../views/AboutView.vue') },
+    // { path: '/services', component: () => import('../views/ServicesView.vue') },
+    // { path: '/blog', component: () => import('../views/BlogView.vue') },
+    // { path: '/contact', component: () => import('../views/ContactView.vue') },
+  ]
 });
 
 export default router;
